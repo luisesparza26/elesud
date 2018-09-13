c6c0cd0488240abbb3addf347c15546998be919b
LuisEsparza <lesparza@esco.mx>
2018-09-12 23:45:54 -0500

    Se añade esstilo para dropdown

--- resources/views/pagelayout.blade.php
+++ resources/views/pagelayout.blade.php
@@ -22,7 +22,10 @@
                     <li class="nav-item"> <a class="nav-link" href="#">Nosotros</a> </li>
                     <li class="nav-item"> <a class="nav-link" href="#">Productos</a> </li>
                     <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Servicios</a>
-                        <div class="dropdown-menu"> <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> </div>
+                      <div class="dropdown-menu" id="drop"> 
+                        <a class="dropdown-item" href="#">Action</a> 
+                        <a class="dropdown-item" href="#">Another action</a> 
+                      </div>
                     </li>
                     <li class="nav-item"> <a class="nav-link" href="#">Proyectos</a> </li>
                     <li class="nav-item"> <a class="nav-link" href="#">Contácto</a> </li>
