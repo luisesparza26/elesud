57721c124cc5cd6cb2f0aeb4fedd7ca8c57892fb
DESKTOP-L1PRK5O\Developer <yhemogollon@gmail.com>
2018-09-17 14:36:16 -0500

    mejoras mejoritas

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,8 +1,8 @@
 @extends( 'pagelayout' )@section( 'content' )
-	<section id="index" class="row">
-		<div class="col-lg-2 p-0 btn-elesud">
+	<div id="index">
+		<div class="btn-elesud">
 			<div class="nav  flex-column align-items-start">
-				<input class="mb-5" type="text" id="panel" readonly disabled>
+				<input class="mb-3" type="text" id="panel" readonly disabled>
 				<a id="homebtn" class="js-scroll-trigger" href="#home" value="INICIO" numero="6">Inicio</a>
 				<a id="aboutbtn" class="js-scroll-trigger" href="#about" value="NOSOTROS" numero="5">Nosotros</a>
 				<a id="productsbtn" class="js-scroll-trigger" href="#products" value="PRODUCTOS" numero="4">Productos</a>
@@ -12,24 +12,26 @@
 				<a id="contactbtn" class="js-scroll-trigger" href="#contact" value="CONTACTO" numero="S1">Contacto</a>
 			</div>
 		</div>
-		<div class="col-lg-10 p-0 index-content">
-			<div id="home" class="section-index">
-				<div class="video-bg">
-					<video autoplay loop muted>
-						<source src="{{ asset('img/index/home.mp4') }}">
-					</video>
-					<div class="home-content"></div>
+		<section id="home" class="section-index">
+			<div class="video-bg">
+				<video autoplay loop muted>
+					<source src="{{ asset('img/index/home.mp4') }}">
+				</video>
+				<div class="home-content">
+					<h1>hola</h1>
 				</div>
 			</div>
-			<div id="about" class="section-index">
+		</section>
+		<section id="about" class="section-index">
+			<div class="container">
 				<div class="p-4 text-about">
 					<h2>Nosotros</h2>
 					<div class="row align-items-center">
+						<div class="col-lg-9">
+							<p>Somos una empresa formada por capitales peruanos, responsable del cumplimiento de las leyes nacionales en transporte vertical, ofreciendo equipos que cumplen con las normativas nacionales e internacionales de equipos de elevación, con el objetivo de garantizar la seguridad de los usuarios. <br><br>Cumplimos con Norma A120 para personas discapacitadas y Reglamento Nacional de Edificaciones - Sección Transporte Mecánico. Contamos con Certificaciones <b>ISO 9001- Calidad</b>.</p>
+						</div>
 						<div class="col-lg-3 py-2">
 							<img src="{{ asset('img/index/sgs.png') }}" alt="SGS">
-						</div>
-						<div class="col-lg-9">
-							<p>Somos una empresa formada por capitales peruanos, responsable del cumplimiento de las leyes nacionales en transporte vertical, ofreciendo equipos que cumplen con las normativas nacionales e internacionales de equipos de elevación, con el objetivo de garantizar la seguridad de los usuarios. <br><br>Cumplimos con Norma A120 para personas discapacitadas y Reglamento Nacional de Edificaciones- Sección Transporte Mecánico. Contamos con Certificaciones <b>ISO 9001- Calidad</b>.</p>
 						</div>
 					</div>
 				</div>
@@ -51,10 +53,21 @@
 					</div>
 				</div>
 			</div>
-			<div id="products" class="section-index">
-			</div>
-			<div id="services" class="section-index">
-				<div class="services-content p-5">
+		</section>
+		<section id="products" class="section-index"></section>
+		<section id="services" class="section-index"></section>
+		<section id="brands" class="section-index"></section>
+		<section id="projects" class="section-index"></section>
+		<section id="contact" class="section-index"></section>
+	</div>
+<!--	<section id="index" class="row">
+
+<div class="col-lg-10 p-0 index-content">
+	<div id="home" class="section-index">
+		<div id="products" class="section-index">
+		</div>
+		<div id="services" class="section-index">
+			<!--				<div class="services-content p-5">
 					<div class="row align-items-center py-5">
 						<div class="col-lg-4">
 							<div class="card">
@@ -74,32 +87,37 @@
 							</div>
 						</div>
 					</div>
-				</div>
-				<div id="brands" class="section-index">
-					<div class="p-4 text-brands">
-						<h2>Representamos a las mejores marcas del mundo</h2>
-						<p>Somos una empresa 100% peruana especialista en trasporte vertical y con más de 15 años de experiencia en el rubro, lo cual nos ha permitido seleccionar a las mejores marcas para cada aplicación a nivel mundial.</p>
-						<div class="row align-items-center py-5">
-							<div class="col-6 col-md-3 m-auto py-3"><img src="{{ asset('img/brands/koyo.png') }}" alt="Representaciones">
+				</div>-->
+			<!--</div>
+			<div id="brands" class="section-index">
+				<div class="p-4 text-brands">
+					<h2>Representamos a las mejores marcas del mundo</h2>
+					<p>Somos una empresa 100% peruana especialista en trasporte vertical y con más de 15 años de experiencia en el rubro, lo cual nos ha permitido seleccionar a las mejores marcas para cada aplicación a nivel mundial.</p>
+					<div class="py-5">
+						<div class="row text-center">
+							<div class="col-6 col-md-6 m-auto py-3"><img src="{{ asset('img/brands/koyo.png') }}" alt="Representaciones">
 							</div>
-							<div class="col-6 col-md-3 m-auto py-3"><img src="{{ asset('img/brands/lehner.png') }}" alt="Representaciones">
+							<div class="col-6 col-md-6 m-auto py-3"><img src="{{ asset('img/brands/lehner.png') }}" alt="Representaciones">
 							</div>
-							<div class="col-6 col-md-3 m-auto py-3"><img src="{{ asset('img/brands/alimak.png') }}" alt="Representaciones">
+						</div>
+						<div class="row text-center">
+							<div class="col-6 col-md-6 m-auto py-3"><img src="{{ asset('img/brands/alimak.png') }}" alt="Representaciones">
 							</div>
-							<div class="col-6 col-md-3 m-auto py-3"><img src="{{ asset('img/brands/hidral.png') }}" alt="Representaciones">
+							<div class="col-6 col-md-6 m-auto py-3"><img src="{{ asset('img/brands/hidral.png') }}" alt="Representaciones">
 							</div>
 						</div>
 					</div>
 				</div>
-				<div id="projects" class="section-index">
-				</div>
-				<div id="contact" class="section-index">
-				</div>
-				<footer class="py-3 bg-dark">
-					<div class="container">
-						<p class="m-0 text-center text-white">Copyright 2018 &copy;Elevadores Sudamericanos S.A.C &#124; Designed by Quickly LA</p>
-					</div>
-				</footer>
 			</div>
-	</section>
-	@endsection
+			<div id="projects" class="section-index">
+			</div>
+			<div id="contact" class="section-index">
+			</div>
+			<footer class="py-3 bg-dark">
+				<div class="container">
+					<p class="m-0 text-center text-white">Copyright 2018 &copy;Elevadores Sudamericanos S.A.C &#124; Designed by Quickly LA</p>
+				</div>
+			</footer>
+		</div>
+	</section>-->
+			@endsection
