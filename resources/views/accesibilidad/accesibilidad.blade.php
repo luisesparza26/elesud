357a92a4eef405d5a5d5e59e0fd732595a7531a8
DESKTOP-L1PRK5O\Developer <yhemogollon@gmail.com>
2018-09-20 11:35:43 -0500

    revisar rutas al dar click al producto

--- resources/views/accesibilidad/accesibilidad.blade.php
+++ resources/views/accesibilidad/accesibilidad.blade.php
@@ -1,105 +1,17 @@
 @extends( 'page' )
-@section( 'title' )Accesibilidad @endsection 
+@section( 'title' )Accesibilidad @endsection
 @section( 'content' )
 	<div id="products-all" class="container">
-		<h2>Accesibilidad</h2>
+		<h1>Accesibilidad<img class="mx-2" src="{{ asset('img/products/accessibility-black.svg') }}" alt="icon" width="35"></h1>
 		<hr role="sub">
 		<div class="row">
-			<div class="col-lg-4 col-md-6">
-				<a href="{{ url('/accesibilidad/alpha') }}">
-					<div class="card my-2">
-						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/alpha/alpha-02.jpg') }}" alt="alpha">
-						<div class="card-body">
-							<h3>Alpha</h3>
-							<p class="card-text">La silla salvaescaleras ALPHA ofrece una solución idónea para el uso interior. Con su apariencia atractiva y diseño cómodo, esta silla salvaescalera le permite subir y bajar escaleras con curvas.</p>
-						</div>
-					</div>
-				</a>
-			</div>
-			<div class="col-lg-4 col-md-6">
-				<a href="{{ url('/accesibilidad/prueba') }}">
-					<div class="card my-2">
-						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/delta/delta-02.jpg') }}" alt="delta">
-						<div class="card-body">
-							<h3>Delta</h3>
-							<p class="card-text">La Delta es la solución perfecta para escaleras rectas, tanto en el interior como en el exterior. La delgadez de su estructura y su silencionsidad son dos rasgos distintivos de su diseño.</p>
-						</div>
-					</div>
-				</a>
-			</div>
-			<div class="col-lg-4 col-md-6">
-				<a href="">
-					<div class="card my-2">
-						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/ikaros/ikaros-02.jpg') }}" alt="ikaros">
-						<div class="card-body">
-							<h3>Ikaros</h3>
-							<p class="card-text">La plataforma salvaescalera Ikarus es la unica plataforma en el mundo para escaleras que cambian la inclinación. Al cambiar la altura de las escaleras la plataforma se ajusta automáticamente su nivel.</p>
-						</div>
-					</div>
-				</a>
-			</div>
-			<div class="col-lg-4 col-md-6">
-				<a href="">
-					<div class="card my-2">
-						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/alpin/alpin-02.jpg') }}" alt="alpin">
-						<div class="card-body">
-							<h3>alpin</h3>
-							<p class="card-text">La plataforma salvaescalera Ikarus es la unica plataforma en el mundo para escaleras que cambian la inclinación. Al cambiar la altura de las escaleras la plataforma se ajusta automáticamente su nivel.</p>
-						</div>
-					</div>
-				</a>
-			</div>
 			<div class="col-lg-4 col-md-6">
 				<a href="">
 					<div class="card my-2">
 						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/omega/omega-02.jpg') }}" alt="omega">
 						<div class="card-body">
-							<h3>omega</h3>
+							<h3>omega<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
 							<p class="card-text">Interiores o exteriores - la Omega se integra naturalmente en cualquier ubicación. Tanto para usos privados como públicos, escaleras rectas o curvadas, el modelo Omega es sinónimo de movilidad y calidad de vida.</p>
-						</div>
-					</div>
-				</a>
-			</div>
-			<div class="col-lg-4 col-md-6">
-				<a href="">
-					<div class="card py-2">
-						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/piscina/piscina-02.jpg') }}" alt="piscina">
-						<div class="card-body">
-							<h3>piscina</h3>
-							<p class="card-text">El poollift Delphin es una valiosa ayuda para entrar o salir de una piscina sin la necesidad de una persona que ayuda. Palancas de mando en la parte superior, así como en el nivel inferior permiten una operación simple de la unidad. </p>
-						</div>
-					</div>
-				</a>
-			</div>
-			<div class="col-lg-4 col-md-6">
-			<a href="">
-					<div class="card my-2">
-						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/public/public-02.jpg') }}" alt="public">
-						<div class="card-body">
-							<h3>public</h3>
-							<p class="card-text">La oruga Public puede transportar todos tipos de sillas de ruedas, tanto electricos como manuales. Se puede subir y bajar escaleras con inclinación hasta 35° de una manera segura y fácil.</p>
-						</div>
-					</div>
-				</a>
-			</div>
-			<div class="col-lg-4 col-md-6">
-			<a href="">
-					<div class="card my-2">
-						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/sa3/sa3-02.jpg') }}" alt="sa3">
-						<div class="card-body">
-							<h3>sa3</h3>
-							<p class="card-text">Con la oruga Standard escaleras rectas con cambio de pendiente pueden ser subido de una manera segura y simple. La oruga se adapta a la mayoria de silla de ruedas. Se puede desmontar fácilmente sin no esta en uso o para transportarlo en un coche. </p>
-						</div>
-					</div>
-				</a>
-			</div>
-			<div class="col-lg-4 col-md-6">
-			<a href="">
-					<div class="card my-2">
-						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/stairmax/stairmax-02.jpg') }}" alt="stairmax">
-						<div class="card-body">
-							<h3>stairmax</h3>
-							<p class="card-text">La oruga Stairmax es la única oruga en el mercado que permite al discapacitado subir y baja escaleras en su propio silla de ruedas. La silla de ruedas del usario requiere una pequeña adaptación a la estructura de la silla. </p>
 						</div>
 					</div>
 				</a>
@@ -109,8 +21,140 @@
 					<div class="card my-2">
 						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/stratos/stratos-02.jpg') }}" alt="stratos">
 						<div class="card-body">
-							<h3>stratos</h3>
+							<h3>stratos<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
 							<p class="card-text">Interiores o exteriores - la plataforma STRATOS se integra naturalmente en cualquier ubicación. Tanto para usos privados como públicos, escaleras rectas o curvadas, el modelo STRATOS es sinónimo de movilidad y calidad de vida.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="{{ url('/accesibilidad/prueba') }}">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/delta/delta-02.jpg') }}" alt="delta">
+						<div class="card-body">
+							<h3>Delta<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">La Delta es la solución perfecta para escaleras rectas, tanto en el interior como en el exterior. La delgadez de su estructura y su silencionsidad son dos rasgos distintivos de su diseño.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="{{ url('/accesibilidad/prueba') }}">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/alpha/alpha-02.jpg') }}" alt="alpha">
+						<div class="card-body">
+							<h3>alpha<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">La silla salvaescaleras ALPHA ofrece una solución idónea para el uso interior. Con su apariencia atractiva y diseño cómodo, esta silla salvaescalera le permite subir y bajar escaleras con curvas.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/essential/essential-02.jpg') }}" alt="essential">
+						<div class="card-body">
+							<h3>essential<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">Instalar una silla subeescalera tipo Essential, es una inversión y una forma de asegurar más su independencia en su hogar.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/liftboy1/liftboy1-02.jpg') }}" alt="liftboy1">
+						<div class="card-body">
+							<h3>Liftboy 1 & 2<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">Cuando las rampas sean demasiado pronunciadas o largas - donde los ascensores conventionales son innecesarios debido a una diferencia de algura demasiado pequeña.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/liftboy5/liftboy5-02.jpg') }}" alt="liftboy5">
+						<div class="card-body">
+							<h3>Liftboy 5<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">Accionamiento eléctrico de tijera, plataforma estable y robusta. Tamaño de la plataforma hasta 1100x1400mm,cuenta con rampa de plegado automática, puerta opcional en la plataforma, el sistema de abre puertas automáticos es opcional.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card py-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/elevex/elevex-02.jpg') }}" alt="elevex">
+						<div class="card-body">
+							<h3>Elevex<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">La plataforma Elevex es una solución simple y económica para instalaciones exteriores para superar las diferencias en altura de hasta 1829 mm.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/alpin/alpin-02.jpg') }}" alt="alpin">
+						<div class="card-body">
+							<h3>alpin<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">Está diseñado para facilitar su uso por las personas mayores o con discapacidad, ocupando un espacio mínimo sin modificaciones estructurales o de obras adicionales.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/delphin/delphin-03.jpg') }}" alt="delphin">
+						<div class="card-body">
+							<h3>delphin<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">El poollift Delphin es una valiosa ayuda para entrar o salir de una piscina sin la necesidad de una persona que ayuda. Palancas de mando en la parte superior, así como en el nivel inferior permiten una operación simple de la unidad.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/stairmax/stairmax-02.jpg') }}" alt="stairmax">
+						<div class="card-body">
+							<h3>stairmax<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">La oruga Stairmax es la única oruga en el mercado que permite al discapacitado subir y baja escaleras en su propio silla de ruedas. La silla de ruedas del usario requiere una pequeña adaptación a la estructura de la silla. </p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/sa3/sa3-02.jpg') }}" alt="sa3">
+						<div class="card-body">
+							<h3>SA-3 & SA-S<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">La oruga se adapta a la mayoria de silla de ruedas. Se puede desmontar fácilmente sin no esta en uso o para transportarlo en un coche. El producto es muy resistente y require solo un mínimo de mantenimiento.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/public/public-02.jpg') }}" alt="public">
+						<div class="card-body">
+							<h3>Public<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">La oruga Public puede transportar todos tipos de sillas de ruedas, tanto electricos como manuales. Se puede subir y bajar escaleras con inclinación hasta 35° de una manera segura y fácil.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-4 col-md-6">
+				<a href="">
+					<div class="card my-2">
+						<img class="card-img-top" src="{{ asset('img/products-all/accesibilidad/pl1400/pl1400-02.jpg') }}" alt="pl1400">
+						<div class="card-body">
+							<h3>PL1400<img src="{{ asset('img/brands/lehner.png') }}" alt="lehner" width="100" height="30"></h3>
+							<p class="card-text">Plataforma vertical movil para acceso a trenes, con elevacion maxima de 1400mm, su funcionamiento es con pilas de 24V</p>
 						</div>
 					</div>
 				</a>
@@ -119,11 +163,19 @@
 	</div>
 <style>
 	#products-all {
-		margin: 4rem auto;
+		margin: 2rem auto;
 	}
 	
 	#products-all a {
 		text-decoration: none;
+	}
+	#products-all h1 {
+		font-size: 2.5rem;
+		font-weight: 300;
+		padding: 2rem 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 	#products-all .card-body {
 		height: 300px;
@@ -131,6 +183,9 @@
 	
 	#products-all .card-body h3 {
 		color: #3d58a1;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 		text-transform: uppercase;
 	}
 	
@@ -140,5 +195,16 @@
 		max-height: 600px;
 		color: #000;
 	}
+	@media (max-width: 1235px) {
+		#products-all .card-body {
+			height: 250;
+		}
+	#products-all .card-body h3 {
+		font-size: 1rem;
+	}
+		#products-all .card-body p {
+			font-size: .9rem;
+		}
+}
 </style>
 @endsection
