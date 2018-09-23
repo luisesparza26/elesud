e9389806fa57262e6f10f4ec53c7153ce939c477
DESKTOP-L1PRK5O\Developer <yhemogollon@gmail.com>
2018-09-23 14:25:23 -0500

    cambios

---
+++ resources/views/contacto-products.blade.php
@@ -0,0 +1,26 @@
+<div id="contact-products">
+	<div>
+		<form class="py-4">
+			<div class="row">
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Nombre">
+				</div>
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Apellido">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Teléfono">
+				</div>
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Correo">
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+<style>
+
+
+</style>
