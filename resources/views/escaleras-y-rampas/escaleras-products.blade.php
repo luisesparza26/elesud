760f4b8c662473b021f19000f99611b6a87b327f
LuisEsparza <lesparza@esco.mx>
2018-09-26 23:50:05 -0500

    Se agregan valores para categoria y productos

--- resources/views/escaleras-y-rampas/escaleras-products.blade.php
+++ resources/views/escaleras-y-rampas/escaleras-products.blade.php
@@ -24,7 +24,7 @@
 					<a class="carousel-control-prev" href="#carousel-products" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a>
 					<a class="carousel-control-next" href="#carousel-products" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a>
 				</div>
-				@include('contacto-products')
+				@include('contacto-products',['categoria' => $product->categoria->nombre,'producto' => $product->titulo])
 			</div>
 			<div class="col-lg-6 py-3">
 				<h1>{{  $product->titulo }}</h1>
